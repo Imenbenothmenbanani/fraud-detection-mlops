2087b51f6c19c6fd187c5315ef92a4f9c68fd46d
Imen BEN OTHMEN BANANI <imenbenothmenbanani@gmail.com>
2025-10-13 12:18:02 +0200

    Deuxieme commit : ajout de eda.py

--- eda.py
+++ eda.py
@@ -1,1 +1,1 @@
-import pandas as pd\ndf = pd.read_csv('data.csv')\nprint(df.head())
+import pandas as pd\ndf = pd.read_csv('data.csv')\nprint('Aperçu des 5 premières lignes :')\nprint(df.head())\nprint('Informations sur le dataset :')\nprint(df.info())
